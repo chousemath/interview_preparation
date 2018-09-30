886b45e400cf27251e88563581bc79875a485408
Joseph Sungpil Choi <chousemath@gmail.com>
2018-10-01 07:16:34 +0900

    Update

--- Largestprimefactor/main.go
+++ Largestprimefactor/main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 )
 
+// time complexity is O(sqrt(n))
+// space complexity is 1
 func main() {
 	num := 600851475143
-	// fmt.Println(sqrt)
 	var maxPrime int
 	for i := 3; i <= num; i += 2 {
 		for num%i == 0 {
 			num /= i
 			maxPrime = i
 		}
-		// fmt.Println("maxPrime:", maxPrime)
 	}
 	fmt.Println("Answer:", maxPrime)
 }
